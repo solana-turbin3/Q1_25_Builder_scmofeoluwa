83184dfd6f3b14cdecfe919a2c00bc50d4fafc40
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2025-02-28 21:46:00 +0100

    removed smartwallet

--- smartwallet/programs/smartwallet/src/helper.rs
+++
@@ -1,5 +0,0 @@
-pub fn calculate_next_reset(current_ts: i64) -> i64 {
-    const SECONDS_PER_DAY: i64 = 86_400;
-    let days_since_epoch = current_ts / SECONDS_PER_DAY;
-    (days_since_epoch + 1) * SECONDS_PER_DAY
-}
